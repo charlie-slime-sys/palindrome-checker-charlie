b8deaec7ff127175d751b1c189012edbccb92a7d
charlie-slime-sys <sawyer.pulsipher24@student.cart.org>
2025-04-09 13:18:01 -0700

    girl what is this

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function checkPalindrome(string) {
+    const len = string.length;
+    for (let i = 0; i < len / 2; i++) {
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+//inp
+const string = prompt('Enter a string: ');
+
+//out
+const value = checkPalindrome(string);
+
+console.log(value);
